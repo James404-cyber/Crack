668e53839cdc4e8f90e2bc902185f14679788715
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-03-27 23:50:51 +0300

    Update librun.py

--- librun.py
+++ librun.py
@@ -7,8 +7,8 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from librun import teaching
+    from Crack import teaching
     teaching()
 elif bit == '32bit':
-    from librun import teaching
+    from Crack import teaching
     teaching()
