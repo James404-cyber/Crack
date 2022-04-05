9df6000aad1adf85d7c45ae1e9e991f57ac72251
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-04-05 11:00:54 +0300

    crack.py

--- crack.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("Crack").teaching()
-elif 'aarch' in arc:
-	__import__("Crack64").teaching()
-else:
-	exit(f' Unknow device machine {arc}')
